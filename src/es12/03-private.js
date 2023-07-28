453d67a0e48419c3618e06913daa2eb46b843266
jdvalmart <juanvalencia9411@outlook.com>
2023-07-28 10:28:46 -0500

    es12, es13

---
+++ src/es12/03-private.js
@@ -0,0 +1,27 @@
+class user {
+    //constructor
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    //metodos
+    #speak() {
+        return 'hello'
+    };
+    greeting() {
+        return `${this.speak} ${this.name}`;
+    }
+
+    get #uAge() {
+        return this.age;
+    }
+
+    set #uAge(n) {
+        this.age = n;
+    }
+}
+
+const bebeloper1 = new user('david', 15);
+console.log(bebeloper1.uAge);
+console.log(bebeloper1.uAge=25);
